d05e5316c8a8f252b0f4232ff2c9ec9cea829950
UJC100 <meetjohnchima@gmail.com>
2025-10-15 18:51:09 +0800

    changed color to green

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -18,7 +18,7 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         gap: 10,
         alignItems: "center",
-        backgroundColor: "red"
+        backgroundColor: "green"
       },
       content: {
         fontSize: 22

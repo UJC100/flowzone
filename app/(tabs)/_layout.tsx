f32e87c65ef50df1dc130a86b6a4c3bdf6b5d560
UJC100 <meetjohnchima@gmail.com>
2025-10-15 18:47:19 +0800

    second commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,42 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+import { View, Text,  } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+import {Ionicons} from '@expo/vector-icons'
+ 
+const TabsLayout = () => {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+    <Tabs 
+    screenOptions={{
+        tabBarActiveTintColor: "red",
+        tabBarInactiveTintColor: "green",
+        tabBarStyle: {
+            backgroundColor:"#1e293b",
+            borderTopWidth: 1,
+            borderTopColor: "yellow",
+            paddingBottom: 30,
+            paddingTop: 10,
+            height: 90
+        },
+        tabBarLabelStyle: {
+            fontSize: 12,
+            fontWeight: "600"
+        },
+        headerShown: false
+    }}
+    >
+        <Tabs.Screen
+        name='index'
+        options={{title: "Todos", tabBarIcon: ({color, size}) => 
+            <Ionicons name='flash-outline' color={color} size={size}/>
+        }}/>
+
+        <Tabs.Screen
+        name='settings'
+        options={{title: "Settings", tabBarIcon: ({color, size}) => 
+            <Ionicons name='settings' color={color} size={size}/>
+        }}/>
     </Tabs>
-  );
+  )
 }
+
+export default TabsLayout
---
+++ app/(tabs)/settings.tsx
@@ -0,0 +1,26 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const SettingsScreen = () => {
+  return (
+    <View>
+      <Text style={styles.constainer}>Settings</Text>
+    </View>
+  )
+}
+
+export default SettingsScreen
+
+
+const styles = StyleSheet.create({
+  constainer: {
+        flex: 1,
+        justifyContent: "center",
+        gap: 10,
+        alignItems: "center",
+        backgroundColor: "red"
+      },
+      content: {
+        fontSize: 22
+      }
+})

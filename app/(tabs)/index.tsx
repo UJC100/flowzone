d2654a1a2f3a362068d1f2bedde1f0a2be90cc02
UJC100 <meetjohnchima@gmail.com>
2025-10-16 00:10:37 +0800

    added dark. mode implmentation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,15 @@
-import { Link } from "expo-router";
-import { Text, View, StyleSheet } from "react-native";
+import useTheme from "@/hooks/useTheme";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 
 export default function Index() {
+  const {toggleDarkMode} = useTheme()
   return (
     <View
       style={styles.constainer}
     >
       <Text style={styles.content}>Edit app/index.tsx to edit this screen.</Text>
       <Text>hi</Text>
+      <TouchableOpacity onPress={toggleDarkMode}><Text>Toggle the mode</Text></TouchableOpacity>
     </View>
   );
 }

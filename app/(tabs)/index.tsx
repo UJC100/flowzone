7fe4401ae1fc86b0fa19791456a4f0973a9b2fe6
UJC100 <meetjohnchima@gmail.com>
2025-10-16 17:01:22 +0800

    Added todo progress bar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,31 +2,38 @@ import { createHomeStyles } from "@/assets/styles/home.styles";
 import { api } from "@/convex/_generated/api";
 import useTheme, { ColorScheme } from "@/hooks/useTheme";
 import { useMutation, useQuery } from "convex/react";
-import { Text, View, StyleSheet, TouchableOpacity, StatusBar } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  StatusBar,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import {LinearGradient} from "expo-linear-gradient"
+import { LinearGradient } from "expo-linear-gradient";
+import Header from "@/components/Header";
 
 export default function Index() {
-  const {toggleDarkMode, colors} = useTheme()
-  const homeStyles = createHomeStyles(colors)
+  const { toggleDarkMode, colors } = useTheme();
+  const homeStyles = createHomeStyles(colors);
 
-  // const todos = useQuery(api.todos.getTodos)
+ 
 
   // const addTodo = useMutation(api.todos.addTodo);
   // const clearAllTodos = useMutation(api.todos.clearAllTodos)
 
   return (
-    <LinearGradient colors={colors.gradients.background} style={homeStyles.container}>
-      <StatusBar barStyle={colors.statusBarStyle}/>
-    <SafeAreaView
-      style={homeStyles.safeArea}
+    <LinearGradient
+      colors={colors.gradients.background}
+      style={homeStyles.container}
     >
-      <Text>hi</Text>
-      <TouchableOpacity onPress={toggleDarkMode}><Text>Toggle the mode</Text></TouchableOpacity>
-    </SafeAreaView>
+      <StatusBar barStyle={colors.statusBarStyle} />
+      <SafeAreaView style={homeStyles.safeArea}>
+        <Header />
+        <TouchableOpacity onPress={toggleDarkMode}>
+          <Text>Toggle the mode</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
     </LinearGradient>
   );
 }
-
-
-
